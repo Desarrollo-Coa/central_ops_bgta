73ae8be238693ec4bd99217882ac554ab03b9a5f
CarlosAndresM <carlosandresmunoza3@gmail.com>
2025-10-23 11:57:06 -0500

    feat: eliminar completamente DigitalOcean Spaces

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'nyc3.digitaloceanspaces.com',
-        port: '',
-        pathname: '/cloud-centralbq-ops-storage/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'nyc3.digitaloceanspaces.com',
-        port: '',
-        pathname: '/cloud-centralctg-ops-storage/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.digitaloceanspaces.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
